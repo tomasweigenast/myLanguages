04cc68865b8d9fad06f0457d6f869e7477833123
Tomas Wegenast <tomaswegenast@gmail.com>
2019-06-10 11:38:36 -0300

    Added test project

---
+++ tests/TestConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyLanguages.Core.Engine;
+using System;
+using System.IO;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalizationEngine.MakeNew();
+
+            LocalizationEngine.Instance.DetectLanguages();
+
+
+            string value = LocalizationEngine.Localizator["Titles.awdawd"];
+            LocalizationEngine.Instance.GetInstalledLanguages();
+            Console.ReadLine();
+        }
+    }
+}
