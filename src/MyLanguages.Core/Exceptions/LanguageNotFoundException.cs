d369a26e40b846df421a3d3b422146f1242e9063
Tomas Wegenast <tomaswegenast@gmail.com>
2019-06-10 11:38:13 -0300

    Added exceptions

---
+++ src/MyLanguages.Core/Exceptions/LanguageNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyLanguages.Core.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when is trying to add a language that does not exists
+    /// </summary>
+    public class LanguageNotFoundException : Exception
+    {
+        /// <summary>
+        /// Creates a new <see cref="LanguageNotFoundException"/> with an error message
+        /// </summary>
+        /// <param name="message">The error message</param>
+        public LanguageNotFoundException(string message) : base(message) { }
+    }
+}
