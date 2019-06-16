4ccd66f79f44d673faee956acab4d7ea56f11a00
Tomas Wegenast <tomaswegenast@gmail.com>
2019-06-16 16:16:28 -0300

    Update project file

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd src
-dotnet restore MyLanguages.Core/MyLanguages.Core.csproj
-dotnet build MyLanguages.Core/MyLanguages.Core.csproj
+dotnet restore
+dotnet build
 msbuild MyLanguages.Wpf/MyLanguages.Wpf.csproj -p:Configuration=Release
 pause
